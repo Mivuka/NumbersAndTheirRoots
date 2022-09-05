603d71562bd2dd146c72f498a852fc87c7bb7998
Mivuka <muokakeinato@gmail.com>
2022-09-05 10:07:06 +0300

    Добавьте файлы проекта.

---
+++ ParallelExamples.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersAndTheirRoots
+{
+
+    public class ParallelExamples
+    {
+        public static async Task DealWithParallelForEachAsync(int start, int end)
+        {
+            var range = Enumerable.Range(start, end);
+            await Parallel.ForEachAsync(range, LongRunningOperationAsync);
+        }
+
+        static void LongRunningOperation(int operationId)
+        {
+            Console.WriteLine($"Root of {operationId} is {Math.Sqrt(operationId)}");
+        }
+
+        static ValueTask LongRunningOperationAsync(int operationId, CancellationToken token)
+        {
+            LongRunningOperation(operationId);
+            return ValueTask.CompletedTask;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//Console.WriteLine("Enter start of range (integer number)");
+//var startRange = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Enter end of range (integer number)");
+//var endRange = int.Parse(Console.ReadLine());
+
+//var range = Enumerable.Range(startRange, endRange);
+
+//var NumberRootPairs = new SortedDictionary<int, double>();
+
+//Parallel.ForEach(range, rootCalc);
+
+//void rootCalc(int number)
+//{
+//    NumberRootPairs.Add(number, Math.Sqrt(number));
+//}
+
+//foreach (var pair in NumberRootPairs)
+//{
+//    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+//}
+using NumbersAndTheirRoots;
+
+Console.WriteLine("Enter start of range (integer number)");
+var startRange = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter end of range (integer number)");
+var endRange = int.Parse(Console.ReadLine());
+
+var NumberRootPairs = new SortedDictionary<int, double>();
+
+await ParallelExamples.DealWithParallelForEachAsync(startRange, endRange);
+
